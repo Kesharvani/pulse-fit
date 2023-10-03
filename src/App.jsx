9f6117a90f7164c48914441d22ebf14d4cb57848
Kesharvani <shubhamkesharvani888@gmail.com>
2023-10-04 02:12:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home";
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
+export default App;
