4c90b8d2d1024d6f3d3e7f39c1922d40a1f74c82
Kesharvani <shubhamkesharvani888@gmail.com>
2023-10-04 20:38:58 +0530

    feat:added sidebar for the app

--- src/common/sidebar/SideBar.jsx
+++ src/common/sidebar/SideBar.jsx
@@ -1,5 +1,38 @@
+import "./SideBar.css";
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 export default function SideBar() {
-  return <div>SideBar</div>;
+  const colorObject = ({ isActive, isPending }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      color: isPending ? "#FFFFF7" : "white",
+      textDecoration: "none",
+    };
+  };
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/" style={colorObject}>
+            Dashboard
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/exercise" style={colorObject}>
+            Exercise
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/food" style={colorObject}>
+            Food
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/goal" style={colorObject}>
+            Goals
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
