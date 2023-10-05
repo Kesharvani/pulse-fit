394d3c0fa543fbd59c369dfd545e5527329529ce
Kesharvani <shubhamkesharvani888@gmail.com>
2023-10-06 02:36:01 +0530

    feat:added modal for food

--- src/pages/food/FoodTracking.jsx
+++ src/pages/food/FoodTracking.jsx
@@ -2,9 +2,12 @@ import "./FoodTracking.css";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { postFoodActionCreator } from "../redux/actions/action";
+import Modal from "react-modal";
+import { AiOutlineClose } from "react-icons/ai";
 export default function FoodTracking() {
-  const data = useSelector((state) => state.food);
+  const food = useSelector((state) => state.food);
   const dispatch = useDispatch();
+  const [modalIsOpen, setIsOpen] = useState(false);
   const [formObject, setFormObject] = useState({
     foodName: "",
     calories: "",
@@ -16,6 +19,31 @@ export default function FoodTracking() {
     const { name, value } = e.target;
     setFormObject((pre) => ({ ...pre, [name]: value }));
   };
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "#242424",
+    },
+  };
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   const handleFoodSubmit = (e) => {
     e.preventDefault();
     dispatch(postFoodActionCreator(formObject));
@@ -26,68 +54,101 @@ export default function FoodTracking() {
       carbohydrates: "",
       fat: "",
     });
+    closeModal();
   };
   return (
-    <div className="container">
-      <h2>Add Food</h2>
-      <form onSubmit={handleFoodSubmit}>
-        <div className="form-group">
-          <label htmlFor="foodName">Food Name:</label>
-          <input
-            type="text"
-            id="foodName"
-            name="foodName"
-            required
-            onChange={inputHandler}
-            value={formObject.foodName}
+    <div className="food_container">
+      <div>
+        <button onClick={openModal} className="food_modal_btn">
+          Add Food
+        </button>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Food Modal"
+        >
+          <AiOutlineClose
+            size={32}
+            className="close_icon"
+            onClick={closeModal}
           />
-        </div>
-        <div className="form-group">
-          <label htmlFor="calories">Calories:</label>
-          <input
-            type="number"
-            id="calories"
-            name="calories"
-            required
-            onChange={inputHandler}
-            value={formObject.calories}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="protein">Protein (grams):</label>
-          <input
-            type="number"
-            id="protein"
-            name="protein"
-            required
-            onChange={inputHandler}
-            value={formObject.protein}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="carbohydrates">Carbohydrates (grams):</label>
-          <input
-            type="number"
-            id="carbohydrates"
-            name="carbohydrates"
-            required
-            onChange={inputHandler}
-            value={formObject.carbohydrates}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="fat">Fat (grams):</label>
-          <input
-            type="number"
-            id="fat"
-            name="fat"
-            required
-            onChange={inputHandler}
-            value={formObject.fat}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+          <div className="container" style={{ backgroundColor: "#242424" }}>
+            <h2>Add Food</h2>
+            <form onSubmit={handleFoodSubmit}>
+              <div className="form-group">
+                <label htmlFor="foodName">Food Name:</label>
+                <input
+                  type="text"
+                  id="foodName"
+                  name="foodName"
+                  required
+                  onChange={inputHandler}
+                  value={formObject.foodName}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="calories">Calories:</label>
+                <input
+                  type="number"
+                  id="calories"
+                  name="calories"
+                  required
+                  onChange={inputHandler}
+                  value={formObject.calories}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="protein">Protein (grams):</label>
+                <input
+                  type="number"
+                  id="protein"
+                  name="protein"
+                  required
+                  onChange={inputHandler}
+                  value={formObject.protein}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="carbohydrates">Carbohydrates (grams):</label>
+                <input
+                  type="number"
+                  id="carbohydrates"
+                  name="carbohydrates"
+                  required
+                  onChange={inputHandler}
+                  value={formObject.carbohydrates}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="fat">Fat (grams):</label>
+                <input
+                  type="number"
+                  id="fat"
+                  name="fat"
+                  required
+                  onChange={inputHandler}
+                  value={formObject.fat}
+                />
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </Modal>
+      </div>
+      <div className="food_card_container">
+        {food?.map((food) => {
+          return (
+            <div className="food_card">
+              <div className="heading">
+                <h3>Food</h3>
+              </div>
+              <div></div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
